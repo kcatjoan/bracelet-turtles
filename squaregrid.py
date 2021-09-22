4428d8acb9805b631fb5e0b9495162afb19de8b9
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-22 09:06:37 -0400

    Update squaregrid.py

--- squaregrid.py
+++ squaregrid.py
@@ -2,11 +2,14 @@ import turtle
 wn = turtle.Screen()
 robin = turtle.Turtle()
 ldistances = [10, 20, 50, 100]
+timesrun = 0 
 
 robin.up()
 robin.speed(3)
 for x in ldistances:
-  robin.forward(x)
-  robin.dot()
-  robin.left(90)
+  while timesrun >= 8:
+    robin.forward(x)
+    robin.dot()
+    robin.left(90)
+    timesrun += 1
 robin.goto(0,0)
