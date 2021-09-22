fba9788d896eb583c97e8acbf8ad7f04dc08c7ab
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-22 10:23:31 -0400

    Update net2.py

--- net2.py
+++ net2.py
@@ -11,7 +11,7 @@ def diagline(strings, distance):
   #All variables have been set.
   
   print(strings)
-  while turtlesdefined <= strings:
+  while turtlesdefined < strings:
     for turtlename in turtlenamelist:
       #Position depending on how many strings already exist.
       turtlename.forward(turtlesdefined*distance)
