30fc2f82ef69fc848e6b63e47c2f4e27897fea27
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-22 10:11:47 -0400

    Update net2.py

--- net2.py
+++ net2.py
@@ -3,6 +3,8 @@ import turtle
 def diagline(strings, distance):
   turtlesdefined = 0
   turtlesweaving = 0
+  [line1, line2, line3, line4, line5, line6, line7, line8, line9, line10] = [turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle(), turtle.Turtle()]
+  #There's gotta be a better way to do that.
   turtlenamelist = [line1, line2, line3, line4, line5, line6, line7, line8, line9, line10]
   #This program maxes out at 10 strings. Do not use more
   #distancemoved = 0. You don't need this because turtle.position() exists. I don't know how to use it yet
