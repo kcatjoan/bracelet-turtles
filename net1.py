4359a467b9b34dcbb7ac0f41edf6771063940e28
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-22 09:26:31 -0400

    Update net1.py

--- net1.py
+++ net1.py
@@ -1,13 +1,18 @@
 import turtle
-line1 = turtle.Turtle()
-numstrings = 5
-def diagline(turtlename): 
+def diagline(turtlename, distance): 
   timesrun = 0
   turtlename.right(45)
   while timesrun < numstrings:
-    turtlename.forward(40)
+    turtlename.forward(distance)
     turtlename.dot()
     timesrun += 1
+
+
+line1 = turtle.Turtle()
+line2 = turtle.Turtle()
+numstrings = 5
+
     
-diagline(line1)
+diagline(line1, 40)
+line2.forward(
 turtle.exitonclick()
