531ac13efe096329f4828ef66554e0368e13c1ad
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-23 12:12:57 -0400

    Update net3.py

--- net3.py
+++ net3.py
@@ -1,6 +1,6 @@
 import turtle
 def doublechevronnet(strings, distance):
   turtledict = {'turtle' + str(anynum): {turtle.Turtle()} for anynum in range(strings)}
-
+  print(turtledict)
   turtle.exitonclick()
 doublechevronnet(5,40)
