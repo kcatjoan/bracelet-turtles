7aa7a4e4906511c5ea0ccd35d983e651c4ffe9fb
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-23 15:19:35 -0400

    Update net3.py

--- net3.py
+++ net3.py
@@ -38,6 +38,8 @@ def doublechevronnet(strings, distance):
         print(str(turtlename) + "at left edge")
         turtlename.left(90)
         turtlename.forward(distance)
+      else:
+        stuckturtles += 1
       
 #         turtlename.left(45)
 #         turtlename.forward(distance)
