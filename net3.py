0f1e95a58534f1f654bac5c82d3b9941a190a8d8
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-23 13:15:51 -0400

    Update net3.py

--- net3.py
+++ net3.py
@@ -23,7 +23,7 @@ def doublechevronnet(strings, distance):
       #positions turtles, leaves "rightedge" as the farthest distance traveled
     #then each turtle (FIGURE  OUT HOW TO HAVE THE TURTLES SPLIT):
   for turtlename in turtlenamelist:
-    list(turtlename.pos())[0] = currentxpos
+    currentxpos = list(turtlename.pos())[0]
     if 0.0 <= currentxpos <= rightedge:
       turtlename.forward(distance)
     elif currentxpos == rightedge:
