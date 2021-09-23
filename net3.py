6b927c89a24a6d2a2724e74c984ac235a4b00eb0
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-23 12:09:01 -0400

    Update net3.py

--- net3.py
+++ net3.py
@@ -1,6 +1,7 @@
 import turtle
 
 stringnum = int(input("string number:")
-turtlenames = {("turtle" + str(num)): turtle.Turtle() for num in 5}
+turtlenames = dict{("turtle" + str(num)): turtle.Turtle() for num in range(5)}
+#dict((k,v) for k,v in list(team.items()) for player in v[‘player’] if player['year'] == 2013)
 #print(turtlenames)
 turtle.exitonclick()
