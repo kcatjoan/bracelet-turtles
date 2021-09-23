74385d2420fe0e996fec221822ade3134d49128f
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-23 12:06:21 -0400

    Update net3.py

--- net3.py
+++ net3.py
@@ -1,5 +1,6 @@
 import turtle
 
-#stringnum = int(input("string number:")
-turtlenames = {("turtle" + str(num)): turtle.Turtle() for num in range(5)}
-print(turtlenames)
+stringnum = int(input("string number:")
+turtlenames = {("turtle" + str(num)): turtle.Turtle() for num in range(stringnum)}
+#print(turtlenames)
+turtle.exitonclick()
