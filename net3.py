665a3d8cae2eed171bb2df11adc736ebd061be80
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-23 12:04:54 -0400

    Update net3.py

--- net3.py
+++ net3.py
@@ -1,5 +1,5 @@
 import turtle
 
-stringnum = int(input("string number:")
-turtlenames = {("turtle" + str(num)): turtle.Turtle() for num in range(stringnum)}
+#stringnum = int(input("string number:")
+turtlenames = {("turtle" + str(num)): turtle.Turtle() for num in range(5)}
 print(turtlenames)
