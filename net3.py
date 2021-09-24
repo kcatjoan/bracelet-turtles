eec41529fe6fa195a0eca98c9a6aa0232bcd7e91
kcatjoan <81879347+kcatjoan@users.noreply.github.com>
2021-09-23 21:06:08 -0400

    Update net3.py

--- net3.py
+++ net3.py
@@ -1,7 +1,8 @@
 import turtle
 import math
 def doublechevronnet(strings, distance): 
-  horizontaldistance = (math.sqrt((distance*distance)/2))
+ # horizontaldistance = (math.sqrt((distance*distance)/2))
+  horizontaldistance = (1/(math.sqrt(2))*distance
   print(str(horizontaldistance) + " is one side of an isosceles right triangle whose hypotenuse is " + str(distance))
   turtlesdefined = 0
   definedturtles = []
